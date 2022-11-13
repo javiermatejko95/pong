0ce44d89c644e713a73e3ae948a09f4ade386d2c
Javier Matejko <javiermatejko@pathfindersstudio.com>
2022-11-13 17:40:33 -0300

    Implemented UI.

---
+++ Pong/Assets/Scripts/Scaler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+
+    #endregion
+
+    #region PRIVATE_FIELDS
+
+    #endregion
+
+    #region UNITY_CALLS
+
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+
+    #endregion
+}
